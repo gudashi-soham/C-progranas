f823734fe37dcf99e8d88ec75d894c0fc60bc113
gudashi-soham <gudashisoham@gmail.com>
2026-04-27 18:23:41 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sub;
+    printf("enter values of a and b:\n");
+    scanf("%d%d",&a,&b);
+    sub=a-b;
+    printf("the sub is:%d",sub);
+    return 0;
+
+}
